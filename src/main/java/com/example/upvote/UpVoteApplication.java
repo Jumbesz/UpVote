21449d230b123e5252064387a7c4fd10fb7fecb3
Schmidt_Balint <schmiti93@gmail.com>
2024-03-19 12:22:42 +0100

    Init Commit Dependencies added Database connection setup

---
+++ src/main/java/com/example/upvote/UpVoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.upvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpVoteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/upvote/models/Profile.java
@@ -0,0 +1,17 @@
+package com.example.upvote.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+}
